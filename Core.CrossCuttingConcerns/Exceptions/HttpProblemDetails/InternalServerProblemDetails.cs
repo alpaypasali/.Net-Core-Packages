06185386eb7d75a3029ac837e5c6adf5c15ec3f0
alpaypasali <alpaypasali@gmail.com>
2023-08-24 03:00:32 +0300

    Exceptions

---
+++ Core.CrossCuttingConcerns/Exceptions/HttpProblemDetails/InternalServerProblemDetails.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
+
+public class InternalServerProblemDetails : ProblemDetails
+{
+    public InternalServerProblemDetails(string detail)
+    {
+        Title = "Internal Server Error";
+        Detail = "Internal Server Error";
+        Status = StatusCodes.Status500InternalServerError;
+        Type = "https://example.com/probs/business";
+
+    }
+
+
+
+}
