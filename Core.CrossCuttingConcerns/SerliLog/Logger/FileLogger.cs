a0f5fdee72589c6d0649b4d324063022f085c977
alpaypasali <alpaypasali@gmail.com>
2023-08-30 00:12:23 +0300

    Logging

---
+++ Core.CrossCuttingConcerns/SerliLog/Logger/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using Core.CrossCuttingConcerns.SerliLog.ConfigurationModels;
+using Core.CrossCuttingConcerns.SerliLog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.SerliLog.Logger;
+
+public class FileLogger:LoggerServiceBase
+{
+    private readonly IConfiguration _configuration;
+
+    public FileLogger(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        FileLogConfiguration logConfig =
+            configuration.GetSection("SeriLogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>()
+            ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        string logFilePath = string.Format(format: "{0}{1}", arg0: Directory.GetCurrentDirectory() + logConfig.FolderPath, arg1: ".txt");
+
+        Logger = new LoggerConfiguration().WriteTo.File(
+            logFilePath, rollingInterval: RollingInterval.Day,//hergun yeni bir dosya oluştur
+            retainedFileCountLimit: null,
+            fileSizeLimitBytes: 5000000,
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}"
+            ).CreateLogger();
+    }
+
+}
